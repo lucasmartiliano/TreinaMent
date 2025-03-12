99395b757323432bf82ee59ca5c53c0cd771c4f2
João Lucas Martiliano da Silva <lucasmartiliano16@gmail.com>
2025-03-12 14:33:15 -0300

    ajustes

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -22,3 +22,20 @@ closeMenu.addEventListener('click', () => {
         openMenu.removeAttribute('style')
     }, 200)
 })
+
+function enviarParaWhatsapp(event) {
+    event.preventDefault();
+    
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let whatsapp = document.getElementById("whatsapp").value;
+    let cnpj_pf = document.getElementById("cnpj_pf").value;
+    let info = document.getElementById("info").value;
+    
+    let mensagem = `Olá, meu nome é ${nome}.\nEmail: ${email}\nWhatsApp: ${whatsapp}\nCNPJ/CPF: ${cnpj_pf}\nInformações adicionais: ${info}`;
+    
+    let numeroWhatsapp = "8293408102";
+    let url = `https://api.whatsapp.com/send?phone=${numeroWhatsapp}&text=${encodeURIComponent(mensagem)}`;
+    
+    window.open(url, "_blank");
+}
